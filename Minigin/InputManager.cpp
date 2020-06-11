dcaa23aab37d42f4abd7423f201c4785fa76fc81
Struan Forsyth <struanforsyth@gmail.com>
2020-06-11 16:06:38 +0200

    Refactor the input manager to work with keyboard & gamepad

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -1,42 +1,127 @@
 #include "MiniginPCH.h"
 #include "InputManager.h"
-#include <SDL.h>
 
+#include <SDL.h>
 
 bool OatmealEngine::InputManager::ProcessInput()
 {
-	ZeroMemory(&m_CurrentState, sizeof(XINPUT_STATE));
-	XInputGetState(0, &m_CurrentState);
+	// Update Gamepad states
+	m_OldGamepadState = m_CurrentGamepadState;
+	ZeroMemory(&m_CurrentGamepadState, sizeof(XINPUT_STATE));
+	XInputGetState(0, &m_CurrentGamepadState);
+
+	// Update keyboard
+	const Uint8* keyboardState = SDL_GetKeyboardState(nullptr);
+
+	std::vector<std::pair<SDL_Keycode, InputTriggerState>> keyState{};
 
 	SDL_Event e;
-	while (SDL_PollEvent(&e)) {
-		if (e.type == SDL_QUIT) {
+	while (SDL_PollEvent(&e))
+	{
+		if (e.type == SDL_QUIT)
 			return false;
-		}
-		if (e.type == SDL_KEYDOWN) {
-			
-		}
-		if (e.type == SDL_MOUSEBUTTONDOWN) {
-			
+		if (e.type == SDL_KEYDOWN && e.key.repeat == 0)
+			keyState.emplace_back(e.key.keysym.sym, InputTriggerState::Pressed);
+		if (e.type == SDL_KEYUP && e.key.repeat == 0)
+			keyState.emplace_back(e.key.keysym.sym, InputTriggerState::Released);
+		
+	}
+
+	for (auto& actionPair : m_InputActions)
+	{
+		auto& currentAction{actionPair.second};
+		currentAction.IsTriggered = false;
+
+		switch (currentAction.TriggerState)
+		{
+		case InputTriggerState::Pressed:
+			// GamePad
+			if (!currentAction.IsTriggered)
+				if (!IsGamepadButtonDown(currentAction.GamepadButtonCode, true) && IsGamepadButtonDown(currentAction.GamepadButtonCode))
+					currentAction.IsTriggered = true;
+			// Keyboard
+			if (!currentAction.IsTriggered)
+				if (IsKeyboardKeyDown(keyState, currentAction))
+					currentAction.IsTriggered = true;
+					
+			break;
+		case InputTriggerState::Down:
+			// GamePad
+			if (!currentAction.IsTriggered)
+				if (IsGamepadButtonDown(currentAction.GamepadButtonCode, true) && IsGamepadButtonDown(currentAction.GamepadButtonCode))
+					currentAction.IsTriggered = true;
+			// Keyboard
+			if (!currentAction.IsTriggered)
+				if (keyboardState[SDL_GetScancodeFromKey(currentAction.KeyboardCode)])
+					currentAction.IsTriggered = true;
+			break;
+		case InputTriggerState::Released:
+			// GamePad
+			if (!currentAction.IsTriggered)
+				if (IsGamepadButtonDown(currentAction.GamepadButtonCode, true) && !IsGamepadButtonDown(currentAction.GamepadButtonCode))
+					currentAction.IsTriggered = true;
+			// Keyboard
+			if (!currentAction.IsTriggered)
+				if (IsKeyboardKeyDown(keyState, currentAction))
+					currentAction.IsTriggered = true;
+			break;
 		}
 	}
 
 	return true;
 }
 
-bool OatmealEngine::InputManager::IsPressed(ControllerButton button) const
+bool OatmealEngine::InputManager::IsActionTriggered(const std::string& actionID)
 {
-	switch (button)
+	return m_InputActions[actionID].IsTriggered;
+}
+bool OatmealEngine::InputManager::AddInputAction(InputAction action)
+{
+	auto it{m_InputActions.find(action.ActionID)};
+	if (it != m_InputActions.end())
+		return false;
+
+	m_InputActions.insert(std::make_pair(action.ActionID, action));
+
+	return true;
+}
+
+bool OatmealEngine::InputManager::IsGamepadButtonDown(GamepadButton button, bool previousFrame)
+{
+	if (button != GamepadButton::INVALID)
 	{
-	case ControllerButton::ButtonA:
-		return m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_A;
-	case ControllerButton::ButtonB:
-		return m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_B;
-	case ControllerButton::ButtonX:
-		return m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_X;
-	case ControllerButton::ButtonY:
-		return m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_Y;
-	default: return false;
+		if (previousFrame)
+		{
+			if (m_OldGamepadState.Gamepad.wButtons == button)
+				return true;
+			if (button == GamepadButton::LEFT_TRIGGER && m_OldGamepadState.Gamepad.bLeftTrigger >= XINPUT_GAMEPAD_TRIGGER_THRESHOLD)
+				return true;
+			if (button == GamepadButton::RIGHT_TRIGGER && m_OldGamepadState.Gamepad.bRightTrigger >= XINPUT_GAMEPAD_TRIGGER_THRESHOLD)
+				return true;
+		}
+		else
+		{
+			if (m_CurrentGamepadState.Gamepad.wButtons == button)
+				return true;
+			if (button == GamepadButton::LEFT_TRIGGER && m_CurrentGamepadState.Gamepad.bLeftTrigger >= XINPUT_GAMEPAD_TRIGGER_THRESHOLD)
+				return true;
+			if (button == GamepadButton::RIGHT_TRIGGER && m_CurrentGamepadState.Gamepad.bRightTrigger >= XINPUT_GAMEPAD_TRIGGER_THRESHOLD)
+				return true;
+		}
 	}
+		
+	return false;
+}
+bool OatmealEngine::InputManager::IsKeyboardKeyDown(const std::vector<std::pair<SDL_Keycode, InputTriggerState>>& keyState, InputAction& currentAction)
+{
+	for (auto& key : keyState)
+	{
+		if (key.second != currentAction.TriggerState)
+			continue;
+
+		if (currentAction.KeyboardCode == key.first)
+			return true;
+	}
+	return false;
 }
 
--- Minigin/InputManager.h
+++ Minigin/InputManager.h
@@ -1,24 +1,85 @@
 #pragma once
-#include <XInput.h>
 #include "Singleton.h"
+
+#include <XInput.h>
+#include <map>
+
+#include "SDL_keycode.h"
 
 namespace OatmealEngine
 {
-	enum class ControllerButton
+	enum class InputTriggerState
 	{
-		ButtonA,
-		ButtonB,
-		ButtonX,
-		ButtonY
+		Pressed,
+		Released,
+		Down
+	};
+
+	enum GamepadButton
+	{
+		A = XINPUT_GAMEPAD_A,
+		B = XINPUT_GAMEPAD_B,
+		X = XINPUT_GAMEPAD_X,
+		Y = XINPUT_GAMEPAD_Y,
+
+		BACK = XINPUT_GAMEPAD_BACK,
+		START = XINPUT_GAMEPAD_START,
+
+		LEFT_SHOLDER = XINPUT_GAMEPAD_LEFT_SHOULDER,
+		LEFT_TRIGGER = 0x0400,	// XINPUT_GAMEPAD_LEFT_TRIGGER
+		RIGHT_SHOLDER = XINPUT_GAMEPAD_RIGHT_SHOULDER,
+		RIGHT_TRIGGER = 0x0800,	// XINPUT_GAMEPAD_RIGHT_TRIGGER
+
+		DPAD_UP = XINPUT_GAMEPAD_DPAD_UP,
+		DPAD_RIGHT = XINPUT_GAMEPAD_DPAD_RIGHT,
+		DPAD_DOWN = XINPUT_GAMEPAD_DPAD_DOWN,
+		DPAD_LEFT = XINPUT_GAMEPAD_DPAD_LEFT,
+
+		LEFT_THUMB_CLICK = XINPUT_GAMEPAD_LEFT_THUMB,
+		RIGHT_THUMB_CLICK = XINPUT_GAMEPAD_RIGHT_THUMB,
+
+		INVALID = 0xA000,
+
+	};
+
+	struct InputAction
+	{
+		InputAction() :
+			ActionID(""),
+			TriggerState(InputTriggerState::Pressed),
+			KeyboardCode(-1),
+			GamepadButtonCode(GamepadButton::INVALID),
+			IsTriggered(false) {}
+
+		InputAction(const std::string& actionID, InputTriggerState triggerState = InputTriggerState::Pressed, SDL_Keycode keyboardCode = -1, GamepadButton gamepadButtonCode = GamepadButton::INVALID) :
+			ActionID(actionID),
+			TriggerState(triggerState),
+			KeyboardCode(keyboardCode),
+			GamepadButtonCode(gamepadButtonCode),
+			IsTriggered(false) {}
+
+		const std::string ActionID;
+		InputTriggerState TriggerState;
+		SDL_Keycode KeyboardCode;
+		GamepadButton GamepadButtonCode;
+		bool IsTriggered;
 	};
 
 	class InputManager final : public Singleton<InputManager>
 	{
 	public:
 		bool ProcessInput();
-		bool IsPressed(ControllerButton button) const;
+		bool AddInputAction(InputAction action);
+		bool IsActionTriggered(const std::string& actionID);
+
 	private:
-		XINPUT_STATE m_CurrentState{};
+		XINPUT_STATE m_OldGamepadState{};
+		XINPUT_STATE m_CurrentGamepadState{};
+		std::map<std::string, InputAction> m_InputActions;
+
+		bool IsGamepadButtonDown(GamepadButton button, bool previousFrame = false);
+		bool IsKeyboardKeyDown(const std::vector<std::pair<SDL_Keycode, InputTriggerState>>& keyState, InputAction& currentAction);
+
 	};
 
 }
